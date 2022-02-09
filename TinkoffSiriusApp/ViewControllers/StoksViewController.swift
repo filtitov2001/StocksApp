3de5a5c733742085405371cbb552b00c0a6ebbb8
Felix Titov <titow.feliks@yandex.ru>
2022-02-10 00:40:00 +0300

    Created UIActivityIndicatorView

--- TinkoffSiriusApp/ViewControllers/StoksViewController.swift
+++ TinkoffSiriusApp/ViewControllers/StoksViewController.swift
@@ -12,7 +12,14 @@ import UIKit
 class StoksViewController: UIViewController {
 
     @IBOutlet var companyNameLabel: UILabel!
+    @IBOutlet var companyNamesPickerView: UIPickerView!
     
+    private let companies: [String: String] = ["Apple": "AAPL",
+                                "Microsoft": "MSFT",
+                                "Google": "GOOG",
+                                "Amazon": "AMN",
+                                "Facebook": "FB" ]
+                                
     override func viewDidLoad() {
         super.viewDidLoad()
         companyNameLabel.text = "Tinkoff"
