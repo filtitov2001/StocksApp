0c5274967c4810e4ef73ca1bf312a1d793bfc902
Felix Titov <titow.feliks@yandex.ru>
2022-02-10 15:53:55 +0300

    Created storyboards for stocks list

---
+++ TinkoffSiriusApp/ViewControllers/StocksListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  StocksListViewController.swift
+//  TinkoffSiriusApp
+//
+//  Created by Felix Titov on 10.02.2022.
+//  Copyright © 2022 by Felix Titov. All rights reserved.
+//  
+
+
+import UIKit
+
+class StocksListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
