be50d9408e111b149309f3c7bfc24f2a6fbdbc4d
Felix Titov <titow.feliks@yandex.ru>
2022-02-11 00:12:33 +0300

    Created stock's model

---
+++ TinkoffSiriusApp/Models/Stock.swift
@@ -0,0 +1,18 @@
+//
+//  Stock.swift
+//  TinkoffSiriusApp
+//
+//  Created by Felix Titov on 10.02.2022.
+//  Copyright © 2022 by Felix Titov. All rights reserved.
+//  
+
+
+import Foundation
+
+struct Stock {
+    let name: String
+    let symbol: String
+    let open: Double
+    let high: Double
+    let low: Double
+}
